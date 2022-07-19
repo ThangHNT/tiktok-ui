c817cea35d386cd7aa8d5b728fe175d7fd109b3c
ThangHNT <thanghn2503@gmail.com>
2022-07-19 22:58:16 +0700

    create menu sidebar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
 import Profile from '~/pages/Profile';
+import Live from '~/pages/Live';
 import { HeaderOnly } from '~/layouts';
 import config from '~/config';
 
@@ -22,6 +23,10 @@ const publicRoutes = [
     {
         path: config.routes.profile,
         component: Profile,
+    },
+    {
+        path: config.routes.live,
+        component: Live,
     },
 ];
 
